9e12adc277131121fd5346f78d4cfd8ff618fdd1
Olga Liubar <olgaliubar@yandex.ru>
2022-05-10 17:52:52 +0200

    move fetching data from routes to catalog

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -2,6 +2,7 @@ import Service from '@ember/service';
 import Band from 'rarwe/models/band';
 import Song from 'rarwe/models/song';
 import { tracked } from 'tracked-built-ins';
+import { isArray } from '@ember/array';
 
 function extractRelationships(object) {
   let relationships = {};
@@ -43,6 +44,29 @@ export default class CatalogService extends Service {
     return records;
   }
 
+  load(response) {
+    return this._loadResource(response.data);
+  }
+
+  async create(type, attributes, relationships = {}) {
+    let payload = {
+      data: {
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+        relationships,
+      },
+    };
+    let response = await fetch(type === 'band' ? '/bands' : '/songs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+    let json = await response.json();
+    return this.load(json);
+  }
+
   _loadResource(data) {
     let record;
     let { id, type, attributes, relationships } = data;
@@ -57,6 +81,36 @@ export default class CatalogService extends Service {
       this.add('song', record);
     }
     return record;
+  }
+
+  async update(type, record, attributes) {
+    let payload = {
+      data: {
+        id: record.id,
+        type: type === 'band' ? 'bands' : 'songs',
+        attributes,
+      },
+    };
+    let url = type === 'band' ? `/bands/${record.id}` : `/songs/${record.id}`;
+    await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+  }
+
+  async fetchRelated(record, relationship) {
+    let url = record.relationships.songs;
+    let response = await fetch(url);
+    let json = await response.json();
+    if (isArray(json.data)) {
+      record[relationship] = this.loadAll(json);
+    } else {
+      record[relationship] = this.load(json);
+    }
+    return record[relationship];
   }
 
   add(type, record) {
